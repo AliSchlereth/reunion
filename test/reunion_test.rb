276f596d0cede9924f0cb43183626ecc68a56edd
Ali Schlereth <aschlere@sbcglobal.net>
2017-07-27 19:28:00 -0600

    Add reunion

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,16 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/reunion'
+require "pry"
 
 class ReunionTest < Minitest::Test
+
+  def test_reunion_is_created_with_a_name
+    reunion = Reunion.new("Denver")
+
+    assert_instance_of Reunion, reunion
+    assert_equal "Denver", reunion.name
+  end
 end
