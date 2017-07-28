674e67cc7302988f84231463edc2ae28c3c23e05
Ali Schlereth <aschlere@sbcglobal.net>
2017-07-27 18:49:46 -0600

    Initial test

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,11 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_takes_a_name
+    activty = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+    assert_equal "Brunch", activity.brunch
+  end
 end
