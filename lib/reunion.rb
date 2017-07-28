d400496d889623a98091103951f4bf58b29c3816
Ali Schlereth <aschlere@sbcglobal.net>
2017-07-27 19:41:01 -0600

    Add reunion total cost

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,4 +11,10 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    activities.reduce(0) do |result, activity|
+      result += activity.total_cost
+    end
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -32,4 +32,36 @@ class ReunionTest < Minitest::Test
 
     assert_equal activity, reunion.activities.last
   end
+
+  def test_add_activity_adds_activities_to_activities
+    reunion = Reunion.new("Denver")
+    activity = Activity.new("Movie")
+    activity.add_participant("Ali", 12)
+    activity.add_participant("Sal", 12)
+    activity_2 = Activity.new("Lunch")
+    activity.add_participant("Ali", 12)
+    activity.add_participant("Sal", 12)
+
+    assert_equal 0, reunion.activities.length
+
+    reunion.add_activity(activity)
+    reunion.add_activity(activity_2)
+
+    assert_equal 2, reunion.activities.length
+  end
+
+  def test_reunion_total_cost_adds_costs_for_all_activities
+    reunion = Reunion.new("San Fran")
+    activity = Activity.new("Hill Walking")
+    activity.add_participant("Ali", 15)
+    activity.add_participant("Sal", 5)
+    activity_2 = Activity.new("Golden Gate")
+    activity.add_participant("Ali", 8)
+    activity.add_participant("Sal", 12)
+
+    reunion.add_activity(activity)
+    reunion.add_activity(activity_2)
+
+    assert_equal 40, reunion.total_cost
+  end
 end
